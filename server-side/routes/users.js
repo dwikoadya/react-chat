6a6222c5c93f4efe3fb75b015f12e96dfacd880a
dwikoadya <dwikoadya@gmail.com>
2021-08-05 16:45:24 +0700

    Add users router

--- server-side/routes/users.js
+++ server-side/routes/users.js
@@ -1,9 +1,120 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const User = require ('../models/user')
+const secret = "sinlay";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/register', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) res.status(500).json({err})
+    res.status(200).json(users)
+  })
+})
+
+router.post("/register", (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let retype = req.body.retype;
+
+  let response = {
+    message: "",
+    data: {},
+    token: "",
+  };
+
+  if (password !== retype) {
+      res.status(500).json({
+      error: true,
+      message: "password does not match",
+    });
+  }
+
+  User.findOne({ email })
+    .then((result) => {
+      if (result) {
+        response.message = "Email Already Exist";
+        res.status(500).json(response);
+      } else {
+        var token = jwt.sign({ email: email }, secret);
+        let user = new User({
+          username: username,
+          email: email,
+          password: password,
+          token: token,
+        });
+        user
+          .save()
+          .then((data) => {
+            (response.message = "Register Success"),
+              (response.data.email = email,
+                response.data.username = username
+              );
+            response.token = token;
+            res.status(200).json(response);
+          })
+          .catch((err) => {
+            res.status(500).json({
+              error: err,
+            });
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        message: "Something Wrong",
+      });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  let response = {
+    message: "",
+    data: {},
+    token: "",
+  };
+
+  User.findOne({ email })
+  .then((data) => {
+    bcrypt.compare(password, data.password)
+    .then((check) => {
+      if (check) {
+        if (data.token) {
+          response.token = data.token;
+          response.data.email = email;
+          response.message = "Login Success";
+          res.status(201).json(response);
+        } else {
+          const newToken = jwt.sign({ email: data.email }, secret);
+          user
+            .updateOne({ email: data.email }, { token: newToken })
+            .then(() => {
+              response.token = newToken;
+              response.data.email = data.email;
+              response.message = "Login Success";
+              res.status(201).json(response);
+            });
+        }
+      } else {
+        response.message = "Login Failed"
+        res.status(500).json(response)
+      }
+    }).catch(err => {
+      console.log(err)
+      response.message = "Login Failed"
+      res.status(500).json(response)
+    })
+  }).catch(err => {
+    console.log(err)
+    response.message = "Email Doesn't Exist"
+    res.status(500).json(response)
+  })
 });
 
 module.exports = router;
