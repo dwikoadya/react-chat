4cd5aa9e8b2961523b040f165a38a83a821274b7
dwikoadya <dwikoadya@gmail.com>
2021-08-09 16:15:39 +0700

    Add Form for Chat

---
+++ client-side/src/components/form/ChatForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "../../css/style.css";
+import User from "../input/User";
+import Message from "../input/Message";
+
+export default class ChatFrom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { nama: "", chat: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    const id = Date.now();
+    this.props.addChat({
+      id,
+      nama: this.state.nama,
+      chat: this.state.chat,
+      sent: true,
+    });
+    this.setState({ nama: "" });
+    this.setState({ chat: "" });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="footer" onSubmit={this.handleSubmit}>
+        <li className="list-group-item borderless d-flex  align-items-center li">
+          <User
+            name="nama"
+            id="nama"
+            placeholder="Your Name"
+            onChange={this.handleChange}
+            value={this.state.nama}
+          />
+          <Message
+            name="chat"
+            placeholder="Write Your CHat Here..."
+            change={this.handleChange}
+            value={this.state.chat}
+          />
+          <div className="speech-bubble">
+            <div className="input-group">
+              <button className="msg_send_btn" type="submit">
+                <i className="fa fa-paper-plane" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+        </li>
+      </form>
+    );
+  }
+}
