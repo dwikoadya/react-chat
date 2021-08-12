353a047cb47db4802c3fb43fab6ffe74f2a4f4a3
dwikoadya <dwikoadya@gmail.com>
2021-08-12 22:42:38 +0700

    Add Chat Item Component

--- client-side/src/components/ChatItem.js
+++ client-side/src/components/ChatItem.js
@@ -1,48 +1,61 @@
-import React from 'react';
-import '../css/style.css'
-import '../css/timeline.css'
-import ReactMarkdown from 'react-markdown';
-// import { randomColor } from './helpers';
+import React from "react";
+import "../css/style.css";
+import "../css/timeline.css";
+import ReactMarkdown from "react-markdown";
 
 export default function ChatItem(props) {
-    // const backgroundColor = randomColor();
-    // const style = {
-    //     backgroundColor,
-    //     padding: '10px 16px',
-    //     textAlign: 'center',
-    //     fontSize: '24px',
-    // }
-    const timestamp = props.chat.id
-    return (
-        <div className="container py-2">
-            <div className="qa-message-list" id="wallmessages">
-                <div className="message-item" id="m16">
-                    <div className="message-inner">
-                        <div className="message-head clearfix">
-                            <div className="avatar pull-left"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT9ATlm4IrNlpf8FhztG3jRTMhTbdqnEUXdIg&usqp=CAU" alt="avatar" /></div>
-                            <div className="user-detail">
-                                <div className="d-flex justify-content-between name d-flex handle" >
-                                    <h5>{props.chat.nama}</h5>
-                                    <button className="btn btn-link px-0 py-0" onClick={props.chat.sent ? props.remove : props.resend} >
-                                        {props.chat.sent ? <i className="fas fa-trash-alt" ></i> : <i className="fas fa-repeat fa-lg"></i>}
-                                    </button>
-                                </div>
-                                <div className="post-meta">
-                                    <div className="asker-meta">
-                                        <span className="qa-message-what"></span>
-                                        <span className="qa-message-when">
-                                            <span className="qa-message-when-data">{ Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit'}).format(timestamp) }</span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="qa-message-content">
-                            <ReactMarkdown source={props.chat.chat} className="mb-0 text-small" />
-                        </div>
-                    </div>
+  const timestamp = props.chat.id;
+  return (
+    <div className="container py-2">
+      <div className="qa-message-list" id="wallmessages">
+        <div className="message-item" id="m16">
+          <div className="message-inner">
+            <div className="message-head clearfix">
+              <div className="avatar pull-left">
+                <img
+                  src="https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png"
+                  alt="avatar"
+                />
+              </div>
+              <div className="user-detail">
+                <div className="d-flex justify-content-between name d-flex handle">
+                  <h5>{props.chat.nama}</h5>
+                  <button
+                    className="btn btn-link px-0 py-0"
+                    onClick={props.chat.sent ? props.remove : props.resend}
+                  >
+                    {props.chat.sent ? (
+                      <i className="fas fa-trash-alt"></i>
+                    ) : (
+                      <i className="fas fa-repeat fa-lg"></i>
+                    )}
+                  </button>
                 </div>
+                <div className="post-meta">
+                  <div className="asker-meta">
+                    <span className="qa-message-what"></span>
+                    <span className="qa-message-when">
+                      <span className="qa-message-when-data">
+                        {Intl.DateTimeFormat("en-US", {
+                          year: "numeric",
+                          month: "2-digit",
+                          day: "2-digit",
+                        }).format(timestamp)}
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
+            <div className="qa-message-content">
+              <ReactMarkdown
+                source={props.chat.chat}
+                className="mb-0 text-small"
+              />
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
